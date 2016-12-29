1ef3293a981eec28f4305cc0e6db87d52aa6b7fe
2020Deception <2020deception@gmail.com>
2016-12-28 23:29:34 -0500

    Initial Commit

---
+++ Pixellator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pixellator
+//
+//  Created by Brian Sharrief Alim Bowman on 12/28/16.
+//  Copyright © 2016 Brian Sharrief Alim Bowman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
