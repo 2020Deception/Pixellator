ae5473b8f7e4974992d132c46973a10ffb273c58
2020Deception <2020deception@gmail.com>
2016-12-31 16:10:46 -0500

    adds script

--- Pixellator/main.m
+++ Pixellator/main.m
@@ -6,12 +6,38 @@
 //  Copyright © 2016 Brian Sharrief Alim Bowman. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
+@import AppKit;
+@import CoreImage;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        if (argc < 3) {
+            printf("not enough args. Pass the image name and the scale value, in their respective order");
+        }
+        NSString *filepath = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+        NSImage *img = [[NSImage alloc] initWithContentsOfFile:filepath];
+        NSImageView *view = [NSImageView imageViewWithImage:img];
+        NSLog(@"%@", img);
+        
+        CIContext *context = [CIContext contextWithOptions:nil];
+        CIImage *ciImage = [[CIImage alloc] initWithData:[NSData dataWithContentsOfFile:filepath]];
+        CIFilter *filter = [CIFilter filterWithName:@"CIPixellate"];
+        
+        [filter setValue:ciImage forKey:kCIInputImageKey];
+        [filter setValue:[CIVector vectorWithCGRect:view.frame] forKey:@"inputCenter"];
+        [filter setValue:@(argv[2]) forKey:@"inputScale"];
+        
+        CIImage *result = [filter valueForKey:kCIOutputImageKey];
+        CGRect extent = [result extent];
+        
+        NSData *imgData = [[NSImage alloc] initWithCGImage:[context createCGImage:result fromRect:extent] size:extent.size].TIFFRepresentation;
+        
+        BOOL success = [imgData writeToFile:[NSString stringWithFormat:@"pixellated-%@", filepath] atomically:NO];
+        
+        if (!success) {
+            printf("something went wrong trying to create the image");
+        }
     }
     return 0;
 }
